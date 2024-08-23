16fdf0c56fb1c2a7c379bdae79262920114ea52a
Igor <80823011+igorwastaken@users.noreply.github.com>
2024-08-23 18:06:10 +0000

    testinf

---
+++ src/pages/redirect/redasp/[id].js
@@ -0,0 +1,27 @@
+export async function getServerSideProps(context) {
+    const data = [
+        {
+            url: 'https://www.educacao.sp.gov.br/educacao-financeira-educacao-inclui-disciplina-no-curriculo-e-1-milhao-de-alunos-vao-aprender-lidar-com-o-dinheiro-em-sala-de-aula/'
+        },
+        {
+            url: 'https://www.cnnbrasil.com.br/economia/financas/educacao-financeira-na-infancia-entenda-qual-a-importancia-e-como-promover/'
+        }
+    ][context.params.id];
+
+    if (!data) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        redirect: {
+            destination: data.url,
+            permanent: true
+        }, // will be passed to the page component as props
+    }
+}
+
